e2a5998ce6b66c9f561ce394f1a98f1f4f03bbb6
avadarad <avadanii@tiscali.it>
2019-02-17 14:00:05 +0100

    made the main modules of the application

---
+++ app.js
@@ -0,0 +1,23 @@
+var budgetController = (function () {
+  
+})();
+
+var UIController = (function () {
+  
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+  document.querySelector('.add__btn').addEventListener('click', function () {
+    // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+  });
+
+})(budgetController, UIController);
