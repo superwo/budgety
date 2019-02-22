265ceda2ff743883d646ceea8c1d841481b4f8e4
avadarad <avadanii@tiscali.it>
2019-02-22 14:21:12 +0100

    adjustet the UI

--- app.js
+++ app.js
@@ -172,6 +172,12 @@ var UIController = (function () {
     return sign + ' ' + int + '.' + dec;
   };
 
+  var nodeListForEach = function (list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function () {
       return {
@@ -236,12 +242,6 @@ var UIController = (function () {
 
     displayPercentages: function (percentages) {
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
-    
-      var nodeListForEach = function (list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
 
       nodeListForEach(fields, function (current, index) {
         if (percentages[index] > 0) {
@@ -262,6 +262,18 @@ var UIController = (function () {
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
     }, 
   
+    changedType: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function (cur) {
+        cur.classList.toggle('red-focus');
+      });
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+    },
 
     getDOMstrings: function () {
       return DOMstrings;
@@ -288,6 +300,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
 
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
   };
 
   var updateBudget = function () {
