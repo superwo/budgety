55d8e27c979e709d49e082316bde06afe72d1bd2
avadarad <avadanii@tiscali.it>
2019-02-22 14:03:40 +0100

    show current month in the title

--- app.js
+++ app.js
@@ -144,7 +144,8 @@ var UIController = (function () {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
   }
 
   var formatNumber = function (num, type) {
@@ -250,6 +251,16 @@ var UIController = (function () {
         }
       });
     },
+
+    displayMonth: function () {
+      var now, year, months, month;
+
+      now = new Date();
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = now.getMonth();
+      year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    }, 
   
 
     getDOMstrings: function () {
@@ -353,6 +364,7 @@ var controller = (function (budgetCtrl, UICtrl) {
   return {
     init: function () {
       console.log('Application has started.');
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalExp: 0,
